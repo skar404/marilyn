e091c536f67a3eedc45ecb1222d53733c9f9cd57
users <denis.malin@publicismedia.ru>
2017-12-15 14:06:25 +0300

    add setup.py

---
+++ setup.py
@@ -0,0 +1,14 @@
+from distutils.core import setup
+
+setup(
+    name='Marilyn',
+    packages=['Marilyn'],  # this must be the same as the name above
+    version='0.0.1',
+    description='Marilyn SDK',
+    author='user',
+    author_email='skar404@gmail.com',
+    url='https://github.com/peterldowns/mypackage',  # use the URL to the github repo
+    download_url='https://github.com/skar404/marilyn/archive/master.zip',  # I'll explain this in a second
+    keywords=['marilyn', 'sdk', 'api'],  # arbitrary keywords
+    classifiers=[],
+)
